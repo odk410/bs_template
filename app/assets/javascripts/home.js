440b0dc80709562113a05dd01c5575a8c9386b72
odk410 <odk410@naver.com>
2018-01-05 17:31:46 +0900

    first commit

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,27 @@
+//= require hs.megamenu
+//= require widget
+//= require widgets/menu
+//= require widgets/mouse
+//= require widgets/slider
+//= require chosen.jquery
+//= require slick
+//= require jquery.fancybox
+
+//= require ui/widget
+//= require ui/version
+//= require ui/keycode
+//= require ui/position
+//= require ui/unique-id
+//= require ui/safe-active-element
+//= require widgets/datepicker
+
+//= require hs.core
+//= require components/hs.header
+//= require helpers/hs.hamburgers
+//= require components/hs.dropdown
+//= require components/hs.slider
+//= require components/hs.select
+//= require components/hs.carousel
+//= require components/hs.popup
+//= require components/hs.datepicker
+//= require components/hs.go-to
